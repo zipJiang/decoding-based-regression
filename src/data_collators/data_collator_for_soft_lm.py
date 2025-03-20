correcting implementation for kl(forward-backward)

src/data_collators/data_collator_for_soft_lm.py
@@ -55,10 +55,12 @@ class DataCollatorForSingleTokenSoftLM(
         self,
         *args,
         sigma: Optional[float] = 0.005,
+        lable_smoothing_factor: Optional[float] = 0.0,
         **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.sigma = sigma
+        self.label_smoothing_factor = lable_smoothing_factor
         self.rank_dict = SingleLabelRankDict.from_tokenizer(self.tokenizer)
         _rank_dict = self.rank_dict.get_rank_dict(tokenizer=self.tokenizer)
         tuples = sorted(_rank_dict.items(), key=lambda x: x[1], reverse=False)
@@ -104,7 +106,10 @@ class DataCollatorForSingleTokenSoftLM(
             first_non_ignore_indices.unsqueeze(-1),
             self.level_ids,
         ] = torch.tensor(probs, dtype=torch.float16)
-        
+
+        # renormalize with the label_smoothing_factor
+        soft_labels = soft_labels * (1 - self.label_smoothing_factor) + self.label_smoothing_factor / vocab_size
+
         # again mask out the ignored_index
         batch['labels'] = soft_labels.masked_fill(~mask.unsqueeze(-1), self.ignore_index)
         

src/dataset_processors/pseudo_labeled_dataset_processor.py
@@ -46,18 +46,6 @@ class PseudoLabeledDatasetProcessor(BaseDatasetProcessor):
 
         assert "unli" not in self._dataset_names, "UNLI dataset should not be in the dataset names."
         
-    def _label_bining(self, x: Union[float, np.ndarray]) -> int:
-        """Same as the one in the UNLIDatasetProcessor
-        """
-        
-        # x is in [1, 10000]
-
-        return max(
-            min(
-                int(x * self._number_of_levels / 10000), self._number_of_levels - 1
-            ), 0
-        )
-
     @overrides
     def _process(self) -> Union[Dataset, DatasetDict]:
         """ """
@@ -132,7 +120,7 @@ class PseudoLabeledDatasetProcessor(BaseDatasetProcessor):
             
             inputs = np.array([d['processed::probability'] for d in dev], dtype=np.float32).reshape(-1, 1)
             # Here we try to use the mapped back values 
-            scores = _inverse_sigmoid_unli(np.array([d['label'] for d in dev], dtype=np.float32))
+            scores = _inverse_sigmoid_unli(np.array([d['label'] for d in dev], dtype=np.float32)) / 10000
             
             ist = IsotonicRegression()
             ist.fit(inputs, scores)
@@ -189,18 +177,6 @@ class PseudoLabeledDatasetProcessor(BaseDatasetProcessor):
     def _create_dataset(self, data: Iterable[dict]) -> Dataset:
         """ """
         
-        def _to_dist(scores: List[float | None]) -> List[float]:
-            dist_ = np.zeros((self._number_of_levels,), dtype=np.float32)
-            for s in scores:
-                if s is not None:
-                    dist_[self._label_bining(s)] += 1
-                else:
-                    # uninformative prior to be added
-                    dist_ = dist_ + (1 / self._number_of_levels)
-                    
-            # normalize the distribution
-            return (dist_ / np.sum(dist_)).tolist()
-            
         return Dataset.from_list([
             {
                 # "premise": item['premise'],
@@ -212,6 +188,7 @@ class PseudoLabeledDatasetProcessor(BaseDatasetProcessor):
                 "completion": self._template.get_completion_template(
                     answer=f" <|label_level_0|>"  # Because this label is always there
                 ),
-                "scores": _to_dist(item['pscores']),
+                # "scores": _to_dist(item['pscores']),
+                "scores": item['pscores'],
             } for item in data
         ])

src/losses/soft_token_loss.py
@@ -15,9 +15,15 @@ class SoftTokenLoss(torch.nn.Module):
 
     _ignore_index: int = -100
     
-    def __init__(self):
+    def __init__(
+        self,
+        reverse_kl_loss: bool = False,
+        temperature: float = 1.0,
+    ):
         """ """
         super().__init__()
+        self._reverse_kl_loss = reverse_kl_loss
+        self._temperature = temperature
         
     def forward(
         self,
@@ -50,11 +56,33 @@ class SoftTokenLoss(torch.nn.Module):
         labels = labels[:, 1:, :].contiguous()
         mask = labels[:, :, 0].eq(self._ignore_index)
         
-        pos_loss = torch.nn.functional.cross_entropy(
-            logits.view(-1, logits.size(-1)),
-            torch.clamp(labels, min=0).view(-1, labels.size(-1)),
-            reduction='none'
-        )
+        # pos_loss = torch.nn.functional.cross_entropy(
+        #     logits.view(-1, logits.size(-1)),
+        #     torch.clamp(labels, min=0).view(-1, labels.size(-1)),
+        #     reduction='none'
+        # )
+        
+        # logits: [batch_size, seq_len, vocab_size]
+        # labels: [batch_size, seq_len, vocab_size]
+        
+        # assume that labels are the soft labels
+        if not self._reverse_kl_loss:
+            pos_loss = torch.nn.functional.kl_div(
+                torch.log_softmax(logits / self._temperature, dim=-1),
+                labels,
+                reduction='none',
+            )
+        else:
+            # reversed kl divergence
+            pos_loss = torch.nn.functional.kl_div(
+                torch.log(labels),
+                torch.log_softmax(logits / self._temperature, dim=-1),
+                log_target=True,
+                reduction="none",
+            )
+
+        # pos_loss: [batch_size, seq_len, vocab_size]
+        pos_loss = pos_loss.sum(dim=-1)
         
         # We only calculate the loss for the non-masked tokens
         pos_loss.masked_fill_(mask.flatten(), 0)

src/tasks/sft_regression_task.py
@@ -4,6 +4,7 @@
 import os
 import torch
 import uuid
+import numpy as np
 from overrides import overrides
 from datasets import load_from_disk
 from accelerate import PartialState
@@ -21,8 +22,9 @@ from typing import (
     Callable
 )
 from registrable import Lazy
-from ..rank_dicts import BaseRankDict
+from ..rank_dicts import BaseRankDict, SingleLabelRankDict
 from ..trainers import DecoderBasedRegressionTrainer
+from ..utils.transforms import _discretize_gaussian
 from ..data_collators import (
     DataCollatorForCompletionRegression,
     DataCollatorForSingleTokenSoftLM
@@ -31,12 +33,9 @@ from ..losses import SingleTokenRegLoss, SoftTokenLoss
 from ..utils.common import get_tokenizer
 
 
-torch.set_printoptions(threshold=256)
-
-
 @BaseTask.register('sft-regression')
 class TrainSFTRegressionTask(BaseTask):
-    __VERSION__ = '0.0.5'
+    __VERSION__ = '0.0.6'
 
     def __init__(
         self,
@@ -44,16 +43,28 @@ class TrainSFTRegressionTask(BaseTask):
         output_dir: Text,
         learning_rate: float,
         model_name: Text,
-        rank_dict: Optional[Lazy[BaseRankDict]] = None,
+        # rank_dict: Optional[Lazy[BaseRankDict]] = None,
         score_loss_func: Optional[Lazy[SingleTokenRegLoss]] = None,
+        label_smoothing_factor: Optional[float] = 0.0,
+        loss_temperature: Optional[float] = 1.0,
+        reverse_kl_loss: Optional[bool] = False,
+        std: float = 0.0,
         force_diffuse: bool = False,
         is_chat: bool = False
     ):
         super().__init__(output_dir=output_dir)
         self._input_dir = input_dir
+        # self._rank_dict = rank_dict.construct(tokenizer=self._tokenizer)
         self._learning_rate = learning_rate
+        self._loss_temperature = loss_temperature
+        self._reverse_kl_loss = reverse_kl_loss
         self._model_name = model_name
         self._is_chat = is_chat
+        
+        # these hyperparameters are used to control the target dist
+        self._label_smoothing_factor = label_smoothing_factor
+        self._std = std
+        
         self._partial_state = PartialState()
         
         self._train_dataset = load_from_disk(os.path.join(self._input_dir, "dataset", 'train'))
@@ -84,20 +95,67 @@ class TrainSFTRegressionTask(BaseTask):
         )
         
         self._tokenizer = get_tokenizer(self._model_name)
+        self._rank_dict = SingleLabelRankDict.from_tokenizer(self._tokenizer)
+        tuples = sorted(self._rank_dict.items(), key=lambda x: x[1], reverse=False)
+        self.levels = np.array([t[1] for t in tuples])
         
+
+        def _score_map(example) -> Dict[Text, Any]:
+            """ """
+            # TODO: map the score to the target dist
+            number_of_levels = len(self._rank_dict)
+            scores = example['scores']
+            if not isinstance(scores, list):
+                scores = [scores]
+            
+            # binning = lambda x: max(
+            #     min(
+            #         int(x * self._number_of_levels / 10000), self._number_of_levels - 1
+            #     ), 0
+            # )
+            
+            filtered_scores = [s for s in scores if s is not None] 
+            if len(filtered_scores) == 0:
+                return np.ones((self._number_of_levels,), dtype=np.float32) / self._number_of_levels
+            
+            scores = _discretize_gaussian(
+                mean=filtered_scores,
+                std=self._std,
+                levels=self.levels[np.newaxis, :]
+            )
+            
+            scores = np.mean(scores, axis=0) * len(filtered_scores) + 0.1 / number_of_levels * (number_of_levels - len(filtered_scores))
+            # renormalize
+            return {
+                "scores": (scores / np.sum(scores)).tolist(),
+            }
+            
         self._score_loss_func = None
         if score_loss_func is not None:
-            # TODO: remove rank_dict dependency
-            self._rank_dict = rank_dict.construct(tokenizer=self._tokenizer)
             # print(self._rank_dict.get_rank_dict(self._tokenizer))
             self._score_loss_func = score_loss_func.construct(
                 rank_dict=self._rank_dict.get_rank_dict(self._tokenizer)
             )
             self._score_loss_func.to(device=self._partial_state.device)
-
+            
+            # TODO: If train with other labels, we need to resolve multiple score circumstances.
+            
         self._compute_loss_func = None
         if force_diffuse:
-            self._compute_loss_func = SoftTokenLoss()
+            self._compute_loss_func = SoftTokenLoss(
+                temperature=self._loss_temperature,
+                reverse_kl_loss=self._reverse_kl_loss,
+            )
+            
+            # convert the score to the target dist
+            self._train_dataset = self._train_dataset.map(
+                _score_map,
+                batched=False,
+            )
+            self._eval_dataset = self._eval_dataset.map(
+                _score_map,
+                batched=False,
+            )
         
         self._trainer = DecoderBasedRegressionTrainer(
             model=self._model,
@@ -106,6 +164,7 @@ class TrainSFTRegressionTask(BaseTask):
                 response_template="### Answer:",
                 tokenizer=self._tokenizer
             ) if not force_diffuse else DataCollatorForSingleTokenSoftLM(
+                label_smoothing_factor=self._label_smoothing_factor,
                 instruction_template="### Question:",
                 response_template="### Answer:",
                 tokenizer=self._tokenizer,
@@ -116,7 +175,7 @@ class TrainSFTRegressionTask(BaseTask):
             train_dataset=self._train_dataset,
             eval_dataset=self._eval_dataset,
             args=SFTConfig(
-                max_seq_length=256,
+                max_seq_length=512,
                 metric_for_best_model="eval_loss",
                 learning_rate=self._learning_rate,
                 num_train_epochs=3,